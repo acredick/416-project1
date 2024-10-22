fcd594b38382f1f937fbb45db2becbf753cf4b2c
acredick <acredick@albany.edu>
2024-10-22 15:38:21 -0400

    begin

---
+++ src/client.java
@@ -0,0 +1,18 @@
+public class client {
+
+     /* METHODS */
+     //todo upload a file from the client to the server, bypassing the cache
+     public void put() {
+
+     }
+
+     //todo download a file to the client using the cache
+     public void get() {
+
+     }
+
+     //todo close the client program
+     public void quit() {
+
+     }
+}
